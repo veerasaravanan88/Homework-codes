78da22f7934be755ac958d13a024e0cf47bf3e23
veerasaravanan88 <ramramghtm@gmail.com>
2026-04-01 17:17:20 +0530

    day 3

---
+++ Marks.java
@@ -0,0 +1,29 @@
+class Marks
+{
+	public static void main(String[]args)
+	{
+		int MARK1= 80;
+		int MARK2= 90;
+		int MARK3= 70;
+		System.out.println("Total=240");
+		System.out.println("Avarage=80");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ data.java
@@ -0,0 +1,55 @@
+class data
+{
+ public static void main(String[]args)
+ {
+	 byte B= 10;
+    short S= 200;
+    int I= 5000;
+    long L=100000l;
+    float F= 5.5f;
+    double D= 123.456;
+    char C= 'Z';
+    boolean OK= false;	
+	System.out.println("BYTE:"+ B);
+    System.out.println("SHORT:+ S");
+    System.out.println("INT:+ I");	
+	System.out.println("LONG:+ L");
+	System.out.println("FLOAT:+ F");	
+	System.out.println("DOUBLE:+ D");
+    System.out.println("CHAR:+ C");
+    System.out.println("BOOLEAN:+ OK");
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
