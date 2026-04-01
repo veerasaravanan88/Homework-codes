3f40aeb07c960b87213d45762e458968e4a2639e
veerasaravanan88 <ramramghtm@gmail.com>
2026-04-01 16:15:30 +0530

    BIO

---
+++ bio.java
@@ -0,0 +1,34 @@
+class information
+{
+	public static void main(String[]args)
+	{
+		String NAME="R.VEERA SARAVANAN";
+		int AGE=20;
+		char GRADE='B';
+		Boolean PASSED=true;
+	    System.out.println("NAME:"+ NAME);
+        System.out.println("AGE:"+ AGE);
+        System.out.println("GRADE:"+ GRADE);		
+		System.out.println("PASSED:"+ PASSED);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
