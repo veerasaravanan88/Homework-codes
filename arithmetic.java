9a697545a163757ccbcce2a7a39cf2f5db9bfae6
veerasaravanan88 <ramramghtm@gmail.com>
2026-04-06 14:08:59 +0530

    day 5

---
+++ arithmetic.java
@@ -0,0 +1,18 @@
+class arithmetic
+{
+	public static void main(String[]args)
+	{
+		int a= 10;
+		int b= 20;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		
+		
+		
+		
+	}
+	
+	
+}
