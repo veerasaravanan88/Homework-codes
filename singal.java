4996f9c9f76efabe3e1e7815fcc5c188b39978f8
veerasaravanan88 <ramramghtm@gmail.com>
2026-04-08 19:02:25 +0530

    day 7

---
+++ singal.java
@@ -0,0 +1,19 @@
+class singal
+{
+	public static void main(String[]args)
+	{
+		char singal='Y';
+		if(singal=='R')
+		{
+			System.out.println("stop");
+		}
+		else if(singal=='Y')
+		{
+			System.out.println("ready");
+		}
+		else if(singal=='G')
+		{
+			System.out.println("go");
+		}
+	}
+}
