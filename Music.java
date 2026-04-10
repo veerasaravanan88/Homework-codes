b3b3341185eb5a75e67b19d4457b269576d42fe2
veerasaravanan88 <ramramghtm@gmail.com>
2026-04-10 19:48:24 +0530

    day 11

---
+++ Music.java
@@ -0,0 +1,206 @@
+import java.util.Scanner;
+class Musicworld
+{
+	public static void main(String [] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("*WELCOME TO THE SPOTIFY*");
+		System.out.println("1.AR.RAHMAN");
+		System.out.println("2.ANIRUDH");
+        System.out.println("3.HARISH JEYARAJ");
+		System.out.println("4.YUVAN");
+		System.out.println("5.ILAIYARAJA");
+		int choice=sc.nextInt();
+		if(choice==1)
+		{
+		  System.out.println("CHOOSE YOUR PLAYLISTS");
+		  System.out.println("1.MELODY");
+		  System.out.println("2.KUTHU");
+		  System.out.println("3.MOTIVATIONAL SONG");
+		  System.out.println("4.SAD");
+		  int option=sc.nextInt();
+		  if(option==1)
+		  {
+			  System.out.println("NETRU ILLADHA MATRAM");
+			  System.out.println("ENNA SOLLA POGIRAI");
+			  System.out.println("ENNAVALE ADI ENNAVALE");
+			  System.out.println("MALARGALE MALARGALE");
+		  }
+		  else if(option==2)
+		  {
+			 System.out.println("THAIYA THAIYA");
+			 System.out.println("MUKKALA MUKKALA");
+			 System.out.println("URVASHI URVASHI");
+			 System.out.println("AATHICHUDI"); 
+		  }
+		  else if(option==3)
+		  {
+			 System.out.println("VANDE MATARAM");
+			 System.out.println("ORUVAN ORUVAN MUDHALALI");
+		     System.out.println("SHAKTHI KODU");
+		     System.out.println("KAPPAL YEARI POOYACHU"); 
+		  }
+		  else if(option==4)
+		  {
+	    	 System.out.println("POONGATRILE");
+			 System.out.println("MARUDHANI");
+			 System.out.println("MINNALAE EN VAZHVIN");
+			 System.out.println("RASATHI");
+			 System.out.println("ORU POIYAVATHU SOL");
+		  }
+          else
+		  {
+			  System.out.println("ADUTHA APP AI NOOKI SELLAVUM");
+		  }
+		}
+        else if(choice==2)
+		{
+		 System.out.println("CHOOSE YOUR PLAYLISTS");
+		 System.out.println("1.MELODY");
+		 System.out.println("2.KUTHU");
+		 System.out.println("3.MOTIVATIONAL SONG");
+		 System.out.println("4.SAD");
+		 int option=sc.nextInt();
+         if(option==1)
+		 {
+		  System.out.println("NEE PAARTHA VIZHIGAL");
+		  System.out.println("POO INDRU NEEYAGA");
+		  System.out.println("VELICHA POOVE");
+		  System.out.println("NEEYUM NAANUM ANBE");
+	     }
+         else if(option==2)
+		 {
+	   	  System.out.println("VAATHI RAID");
+		  System.out.println("NAA READY");
+	      System.out.println("BADASS");
+		  System.out.println("VANTHA EDAM");
+		 }			 
+		 else if(option==3)
+		 {
+		  System.out.println("VELAI ILLA PATATHAARI");
+		  System.out.println("BOOMI ENNA SUTHUTHEY");
+		  System.out.println("ETHIL NEECHAL");
+	      System.out.println("SURVIVA");
+		 }	
+         else if(option==4)
+		 {
+		  System.out.println("PORKANDA SINGAM");
+		  System.out.println("KANAVE KANAVE");
+		  System.out.println("KANANAANA KANNE");
+	      System.out.println("JODI NILAVE");		  
+		 }			 
+         else
+		 {
+			 System.out.println("ADUTHA AAP AI NOKKI SELLAVUM");
+		 }			 
+		}
+        else if(choice==3)
+		{
+		 System.out.println("CHOOSE YOUR PLAYLISTS");
+		 System.out.println("1.MELODY");
+		 System.out.println("2.KUTHU");
+		 System.out.println("3.SAD");
+		 int option=sc.nextInt();
+         if(option==1)
+		 {
+		  System.out.println("UNNALE UNNALE");
+		  System.out.println("ANUL MELLAE");
+		  System.out.println("YETHO ONDRU");
+		  System.out.println("VASEEGARA");
+		  System.out.println("PAARTHA MUDHAL");
+	     }
+         else if(option==2)
+		 {
+	   	  System.out.println("TIRUNELVELI ALWA DA");
+		  System.out.println("AARADI KATRE");
+	      System.out.println("SODA BOTTLE");
+		  System.out.println("ARUVA MEESAI");
+		 }			 
+		 else if(option==3)
+		 {
+		  System.out.println("VENMATHIYE");
+		  System.out.println("OH MANAMAE");
+		  System.out.println("YEMMA YEMMA");
+	      System.out.println("OTHAYILE");
+		 }	
+         else 
+		 {
+		  System.out.println("ADHUTHA APP AI NOKKI SELLAVUM");
+		 }
+		}
+        else if(choice==4)
+		{
+		 System.out.println("CHOOSE YOUR PLAYLISTS");
+		 System.out.println("1.MELODY");
+		 System.out.println("2.KUTHU");
+		 System.out.println("3.MOTIVATIONAL SONG");
+		 System.out.println("4.SAD");
+		 int option=sc.nextInt();
+         if(option==1)
+		 {
+		  System.out.println("AATHADI MANASUTHAN");
+		  System.out.println("CHINNA SIRASU");
+		  System.out.println("VENMEGAM");
+		  System.out.println("IYAIYAIYOO");
+	     }
+         else if(option==2)
+		 {
+	   	  System.out.println("MACHI OPEN THE BOTTLE");
+		  System.out.println("SAROJA SAMAN NIKALO");
+	      System.out.println("NO MONEY NO HONEY");
+		  System.out.println("VILAIYADU MANKATHA");
+		 }			 
+		 else if(option==3)
+		 {
+		  System.out.println("ETHIRTHU NILL");
+		  System.out.println("ORU NALIL");
+		  System.out.println("NIMIRNTHU NIL");
+	      System.out.println("UNAKULLAE MIRUGAM");
+		 }	
+		 else if(option==4)
+		 {
+		  System.out.println("KADHAAL VALARTHEN");
+		  System.out.println("YETHO ONDRU ENNAI");
+		  System.out.println("POOGATHEY");
+	      System.out.println("NINAITHU NENAITHU");
+		 }	
+         else 
+		 {
+		  System.out.println("ADHUTHA APP AI NOKKI SELLAVUM");
+		 }
+		}
+		else if(choice==5)
+		{
+		 System.out.println("CHOOSE YOUR PLAYLISTS");
+		 System.out.println("1.MELODY");
+		 System.out.println("2.KUTHU");
+		 System.out.println("3.SAD");
+		 int option=sc.nextInt();
+         if(option==1)
+		 {
+		  System.out.println("ORU KILLI URUGUTHU");
+		  System.out.println("KUZHALOOTHUM KANANUKKU");
+		  System.out.println("ENNAI THOTTU");
+		  System.out.println("RAJA RAJA THAN");
+	     }
+         else if(option==2)
+		 {
+	   	  System.out.println("OORUVITTU OORUVANTHU");
+		  System.out.println("NOORU VARUSHAM");
+	      System.out.println("AASAI NOORUVAGAI");
+		  System.out.println("ASSAI ATHIGAM VACHU");
+		 }			 
+		 else if(option==3)
+		 {
+		  System.out.println("KANNE KALAIMAANE");
+		  System.out.println("THENE THENPANDI NEEYE");
+		  System.out.println("PAADI PARANTHA KILI");
+	      System.out.println("NILAVE VAA");
+		 }	
+		 else
+		 {
+			System.out.println("ADUTHA APP AI NOOKI SELLAVUM"); 
+		 }
+		}
+	}
+}
