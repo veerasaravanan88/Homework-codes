484497e4927378c2bb5f123b5d3321ffe56c6d03
veerasaravanan88 <ramramghtm@gmail.com>
2026-04-18 12:36:25 +0530

    day 15

---
+++ h2.java
@@ -0,0 +1,16 @@
+public class h2 
+{
+    public static void main(String[] args) 
+	{
+        char c='A';
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+			System.out.print(c+" ");
+			 	c++;
+			}
+			System.out.println(" ");
+		}
+    }
+}
