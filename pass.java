f59d719575c961286e5999c530ef0cf3a6ba22a2
veerasaravanan88 <ramramghtm@gmail.com>
2026-05-05 16:28:24 +0530

    pass by value and referance

---
+++ pass.java
@@ -0,0 +1,22 @@
+class pass
+{
+	public static void main(String[]args)
+	{
+		int a=10;
+		int b=20;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(a+b);
+		pass p1=new pass();
+		p1.displayall(a,b);
+		
+	}
+	void displayall(int a,int b)
+	{
+		a=1005;
+		b=200;
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+}
